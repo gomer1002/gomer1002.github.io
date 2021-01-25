e11df3ff7c4daa72833b66852b5c9b3dbcac53cc
gomer1002 <chubaka1002@gmail.com>
2021-01-25 23:50:46 +0700

    .

--- script.js
+++ script.js
@@ -1,8 +1,68 @@
 // get jsons on page load
 window.onload = function () {
 	//httpGetJson();
-	httpGetRepos();
+	//httpGetRepos();
+	httpGetAnimes();
 }
+
+//shikimori api
+// v1 https://shikimori.one/api/doc/1.0
+// v2 https://shikimori.one/api/doc
+// ouath guide https://shikimori.one/oauth
+
+// APPLICATION_NAME		githubParser
+// CLIENT_ID 			EQ7bdEXo-_euNVEUsVpyOOwn2cJg4AydKUylAPHhvGY
+// CLIENT_SECRET 		dJv6XFrKXbIDDNZKHeVDu4f4olqeCywmjGrXH24iWj4
+// REDIRECT_URI 		urn:ietf:wg:oauth:2.0:oob
+
+// link for AUTORIZATION_CODE 
+// https://shikimori.one/oauth/authorize?client_id=EQ7bdEXo-_euNVEUsVpyOOwn2cJg4AydKUylAPHhvGY&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=
+
+/* sample
+curl -X POST "https://shikimori.one/oauth/token" \
+-H "User-Agent: APPLICATION_NAME" \
+-F grant_type="authorization_code" \
+-F client_id="CLIENT_ID" \
+-F client_secret="CLIENT_SECRET" \
+-F code="AUTORIZATION_CODE" \
+-F redirect_uri="REDIRECT_URI"
+*/
+
+/* with my data
+curl -X POST "https://shikimori.one/oauth/token" \
+-H "User-Agent: githubParser" \
+-F grant_type="authorization_code" \
+-F client_id="EQ7bdEXo-_euNVEUsVpyOOwn2cJg4AydKUylAPHhvGY" \
+-F client_secret="dJv6XFrKXbIDDNZKHeVDu4f4olqeCywmjGrXH24iWj4" \
+-F code="Iglxu9rWlzGOv15N5ROFvcDrJ9kyIhtLKOI30X-5pBI" \
+-F redirect_uri="urn:ietf:wg:oauth:2.0:oob"
+*/
+
+/*
+check user_id and other stuff
+https://shikimori.one/api/users/whoami
+
+get anime info by id
+https://shikimori.one/api/animes/*id anime(target_id)*
+
+get anime list by user id
+https://shikimori.one/api/v2/user_rates?user_id=397613
+info
+https://shikimori.one/api/doc/2.0/user_rates/index
+*/
+function httpGetAnimes() {
+	let auth = new XMLHttpRequest();
+	//auth.open('POST', 'https://shikimori.one/oauth/token', true);
+	//
+	auth.open('POST', 'https://shikimori.one/oauth/authorize?client_id=EQ7bdEXo-_euNVEUsVpyOOwn2cJg4AydKUylAPHhvGY&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=', false);
+	//auth.send(null);
+	console.log(auth.status);
+	console.log(auth.responseText);
+	//auth.setRequestHeader('User-Agent', 'githubParser');
+	//auth.setRequestHeader('grant_type', 'authorization_code';
+	//auth.setRequestHeader('client_id', this.clientID);
+}
+
 
 //github api sample
 function httpGetRepos() {
